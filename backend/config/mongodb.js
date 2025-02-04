f972b1e1697b1849d053be24c15b565d52c5c3a5
tosif66 <motosifansari66@gmail.com>
2025-02-04 07:45:44 +0530

    #

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,9 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     mongoose.connection.on("connected", () => {
-        console.log("MongoDB connected successfully")
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/Feedback_Portal`)
+        console.log("MongoDB connected successfully");
+    });
+    mongoose.connection.on("error", (err) => {
+        console.error("MongoDB connection error:", err);
+    });
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/Feedback_Portal`).catch(err => {
+        console.error("Failed to connect to MongoDB:", err);
+    });
+
 }
 
 export default connectDB;
